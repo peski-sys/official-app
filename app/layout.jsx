e4aaa20c31c3990a2c9ebd6792e8b3296b14521b
peski-sys <saugatkhanal100@gmail.com>
2024-01-10 00:10:01 +0545

    ignore

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,8 @@
-import { Poppins } from 'next/font/google'
-import './globals.css'
+import { Poppins } from 'next/font/google';
+import './globals.css';
 import { ClerkProvider } from "@clerk/nextjs";
 
-const pop = Poppins({ weight: '500',subsets: ['latin'] })
+const pop = Poppins({ weight: '500',subsets: ['latin'] });
 
 export const metadata = {
   title: 'CarsAI | AI Sales SaaS to get the most out of your audio',
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/images/dashicon.ico" sizes="any" />
+      <link rel="icon" href="/images/dashicon.ico"/>
       </head>
       <ClerkProvider>
       <body className={pop.className}>
