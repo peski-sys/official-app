3530b9f67c19894157d5c43d6ffe4bc047e407e0
peski-sys <saugatkhanal100@gmail.com>
2024-01-09 18:13:41 +0545

    minor deletion

--- components/dashpage/sidebar.jsx
+++ components/dashpage/sidebar.jsx
@@ -14,7 +14,7 @@ export default async function sidebar() {
             <div className="flex flex-col flex-grow pt-5 overflow-y-auto border-r bg-neutral-800 ">
                 <div className="flex flex-col items-center flex-shrink-0 px-4 ">
                     <Link className="px-8 text-left focus:outline-none" href="/dashboard">
-                        <h2 className="block p-2 text-xl font-medium tracking-tighter transition duration-500 ease-in-out transform cursor-pointer text-neutral-200 hover:text-neutral-200">Dashboard</h2>
+                        <h2 className="block p-2 text-xl font-medium tracking-tighter transition duration-500 ease-in-out transform cursor-pointer text-white hover:text-white">Dashboard</h2>
                     </Link>
                     <button className="hidden rounded-lg focus:outline-none focus:shadow-outline">
                         <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6">
@@ -27,7 +27,7 @@ export default async function sidebar() {
                     <nav className="flex-1 space-y-1 bg-neutral-800">
                         <ul>
                             <li>
-                                <Link className="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform border rounded-lg text-neutral-200 border-neutral-800 hover:border-neutral-800 focus:shadow-outline hover:bg-neutral-900" white="" href="/dashboard">
+                                <Link className="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform border rounded-lg text-white border-neutral-800 hover:border-neutral-800 focus:shadow-outline hover:bg-neutral-900" white="" href="/dashboard">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
                                     </svg> 
@@ -35,7 +35,7 @@ export default async function sidebar() {
                                 </Link>
                             </li>
                             <li>
-                                <Link className="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform border rounded-lg text-neutral-200 border-neutral-800 hover:border-neutral-800 focus:shadow-outline hover:bg-neutral-900" href="/dashboard/upload">
+                                <Link className="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform border rounded-lg text-white border-neutral-800 hover:border-neutral-800 focus:shadow-outline hover:bg-neutral-900" href="/dashboard/upload">
                                 <svg data-slot="icon" className="w-4 h-4" fill="none" strokeWidth="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"></path>
                                 </svg>
@@ -43,7 +43,7 @@ export default async function sidebar() {
                                 </Link>
                             </li>
                             <li>
-                                <Link className="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform border rounded-lg text-neutral-200 border-neutral-800 hover:border-neutral-800 focus:shadow-outline hover:bg-neutral-900" href="/dashboard/status">
+                                <Link className="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform border rounded-lg text-white border-neutral-800 hover:border-neutral-800 focus:shadow-outline hover:bg-neutral-900" href="/dashboard/status">
                                     <svg data-slot="icon" className="w-4 h-4" fill="none" strokeWidth="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"></path></svg>
                                     <span className="ml-4">All Records</span>
                                 </Link>
@@ -57,13 +57,13 @@ export default async function sidebar() {
                                     <span className="ml-4">Settings</span></Link>
                             </li> */}
                         </ul>
-                        <p className="px-4 pt-4 font-medium uppercase text-neutral-200 ">Recent Five</p>
+                        <p className="px-4 pt-4 font-medium uppercase text-white ">Recent Five</p>
                         <ul>
                             {nums.map(item => {
                                 let path = `/dashboard/audio/${item}`
                                 return (
                                     <li key={item}>
-                                    <Link className="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform border rounded-lg text-neutral-200 border-neutral-800 hover:border-neutral-800 focus:shadow-outline hover:bg-neutral-900" white="" href={path}>
+                                    <Link className="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform border rounded-lg text-white border-neutral-800 hover:border-neutral-800 focus:shadow-outline hover:bg-neutral-900" white="" href={path}>
                                         <svg data-slot="icon" className="w-4 h-4" fill="none" strokeWidth="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z"></path>
                                         </svg>
